fd736f3f44e1a18b5a6439b48c2cc77e946e1d74
abarkera4 <abarkera4@gmail.com>
2024-06-18 19:50:49 -0500

    Push to make autograde run

--- candidate-testing.js
+++ candidate-testing.js
@@ -15,7 +15,6 @@ let questions = ["Who was the first American woman in space? ", "True or false: 
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"] ;
 let candidateAnswers = [];
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
 
