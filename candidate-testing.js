d5bac3a05815e334dee590380646898ce20b8dba
abarkera4 <abarkera4@gmail.com>
2024-07-01 09:59:57 -0500

    Fixed output for test failed condition.

--- candidate-testing.js
+++ candidate-testing.js
@@ -9,7 +9,6 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
-
 //TODO: Variables for Part 2
 let questions = ["Who was the first American woman in space? ", "True or false: 5 kilometer == 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
 let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"] ;
@@ -56,7 +55,7 @@ function gradeQuiz(candidateAnswers) {
   if (grade >= 80){
     console.log(">>> Status: PASSED <<<")
   } else {
-    console.log(">>> Status : FAILED<<< ")
+    console.log(">>> Status : FAILED <<< ")
   }
 
   return grade;
